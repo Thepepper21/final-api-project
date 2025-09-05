0ace55323338e7a1f22eca2cf44f4c9408457a5f
Thepepper17 <aicsraambrad@gmail.com>
2025-09-06 05:56:22 +0800

    api route

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ImageController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('images')->group(function () {
+    Route::get('/', [ImageController::class, 'index']);
+    Route::post('/', [ImageController::class, 'store']);
+    Route::get('/{image}', [ImageController::class, 'show']);
+    Route::get('/{image}/file', [ImageController::class, 'serve']);
+    Route::put('/{image}', [ImageController::class, 'update']);
+    Route::patch('/{image}', [ImageController::class, 'update']);
+    Route::delete('/{image}', [ImageController::class, 'destroy']);
+});
+
+
