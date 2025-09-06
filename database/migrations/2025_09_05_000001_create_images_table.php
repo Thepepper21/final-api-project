27edfbc4e86cbe3912fb8662d5fc19094140e5da
madelynabaculi99-ops <madelynabaculi99@gmail.com>
2025-09-06 08:41:38 +0800

    fix bug

--- database/migrations/2025_09_05_000001_create_images_table.php
+++ database/migrations/2025_09_05_000001_create_images_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // already created a table brad
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
